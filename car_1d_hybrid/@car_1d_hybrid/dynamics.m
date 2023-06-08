db09b7d096f719521f4c6b03ca7d84a6db04fa7f
psrobotics <psrobotics@foxmail.com>
2023-06-08 16:12:34 -0700

    add: 0608 obst dyn limit

--- car_1d_hybrid/@car_1d_hybrid/dynamics.m
+++ car_1d_hybrid/@car_1d_hybrid/dynamics.m
@@ -28,7 +28,7 @@ else
   dx = zeros(obj.nx, 1);
   switch q_mode
       case 1 % operation mode 1
-          dx(1) = 4*u(1) + d(1);
+          dx(1) = 4*u(1).*is_in_obst(x, obj.obst_range) + d(1);
       case 2 % operation mode 2
           dx(1) = 1*u(1) + d(1);
   end
@@ -41,10 +41,28 @@ function dx = dynamics_cell_helper(obj, x, u, d, q_mode, dims, dim)
 
 switch q_mode
     case 1 % operation mode 1
-    dx = 4*u{1} + d{1};
+        u{1};
+    dx = 4*u{1}.*is_in_obst(x, obj.obst_range) + d{1}; % mod
+    dx;
     case 2 % operation mode 2
     dx = 1*u{1} + d{1};
   otherwise
     error('Only 2 operation modes available')
 end
+end
+
+%%
+function in_obst_arr = is_in_obst(x, obst_range)
+x_value = x{1}; % x is a 1x1 cell with double matrix inside
+in_state_size = size(x_value);
+in_obst_arr = ones(in_state_size);
+
+for i = 1:in_state_size
+    if x_value(i)>obst_range(1) && x_value(i)<obst_range(2)
+        in_obst_arr(i) = 0; % add velocity limit if in obst range
+    end
+end
+
+%in_obst_arr
+
 end
